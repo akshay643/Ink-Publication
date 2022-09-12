f4c4a902f597095b2c701356a8d2144f868bf0e4
akshay643 <akshay.thakur643@gmail.com>
2022-09-12 19:31:19 +0530

    my changes

--- src/components/Research/CardContainer.js
+++ src/components/Research/CardContainer.js
@@ -16,7 +16,9 @@ function CardContainer() {
         <h1 className="card-container-heading  text-center">
           How to <span className="card-container-heading-2">proceed</span>{' '}
         </h1>
-
+        <div className="d-flex justify-content-end fs-2">
+          <i className="fa-solid fa-arrow-right-long ms-auto"></i>
+        </div>
         <div className="card-main-container d-flex justify-content-between overflow-scroll">
           <Cards className="ms-2" title="Research Guidance" image={Img1} />
           <Cards className="ms-2" title="Research Publication" image={Img3} />
